9b2013cff2281de86c19a0f23e769721e51d122a
Henry McIntosh <henry@bluelyon.tech>
2017-08-21 00:03:34 +0100

    changed page.js

--- page.js
+++ page.js
@@ -3,5 +3,4 @@ document.addEventListener('DOMContentLoaded',() => {
   navigator.serviceWorker.register('/service-worker.js')
      .then(reg => console.log('SW registered!', reg))
      .catch(err => console.log('Boo!', err));
-
 });
